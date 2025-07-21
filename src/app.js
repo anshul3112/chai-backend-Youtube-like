795545607648680de60103befdf630dfd757f452
anshul3112 <singhanshul3112@gmail.com>
2025-07-21 17:55:57 +0530

    router start

--- src/app.js
+++ src/app.js
@@ -19,4 +19,18 @@ app.use(express.static("public")); // to store assets in public
 
 app.use(cookieParser());
 
+
+//routes import :
+
+import userRouter from './routes/user.routes.js';
+
+// routes decleration:
+// app.get -> when not using routed
+// now it is mandatory to use middleware
+
+app.use('/api/v1/users' , userRouter);
+// https://localhost:8000/api/v1/users/register
+
+
+       
 export {app};
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,9 @@
-import asyncHandler from '../utils/asyncHandler.js'
+import {asyncHandler} from '../utils/asyncHandler.js'
 
+const registerUser = asyncHandler(async(req,res) => {
+    res.status(200).json({
+        message : "OK"
+    }) 
+})
 
-
+export {registerUser};
